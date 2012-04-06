74a90739ea6f97d174c00ce3013698ac4e7a27b0
Zeray Rice <fanzeyi1994@gmail.com>
2012-04-06 17:39:44 +0800

    finish bookmark

--- wobbuffet.py
+++ wobbuffet.py
@@ -2,12 +2,17 @@
 # AUTHOR: Zeray Rice <fanzeyi1994@gmail.com>
 # FILE: wobbuffet.py
 # CREATED: 15:57:06 06/04/2012
-# MODIFIED: 16:22:30 06/04/2012
+# MODIFIED: 17:36:02 06/04/2012
+
+import datetime
+from functools import wraps
 
 from flask import Flask
+from flask import abort
 from flask import request
 from flask import render_template
 from flaskext.sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -26,6 +31,15 @@ try:
 except Exception:
     pass
 
+def authentication(f):
+    @wraps(f)
+    def _auth_decorator(*args, **kwargs):
+        auth = request.args.get("auth", type=unicode)
+        if auth != app.config["AUTH"]:
+            return abort(404)
+        return f(*args, **kwargs)
+    return _auth_decorator
+
 @app.route("/")
 def index():
     page = request.args.get("p", 0, type=int)
@@ -33,6 +47,34 @@ def index():
     link_count = links.count()
     links = links.limit(app.config["LINKS_PER_PAGE"]).offset(page * app.config["LINKS_PER_PAGE"]).all()
     return render_template("index.html", links = links)
+
+@app.route("/add", methods=["POST", "GET"])
+@authentication
+def add_link():
+    if request.method == 'POST':
+        return post_link()
+    url = request.args.get("link", type=unicode)
+    title = request.args.get("title", type=unicode)
+    auth = request.args.get("auth", type=unicode)
+    if not url and title:
+        return abort(400)
+    return render_template("new.html", url = url, title = title, auth = auth)
+
+def post_link():
+    print str(request.form)
+    url = request.form["url"]
+    title = request.form["title"]
+    comment = request.form["comment"]
+    if not url and title:
+        return render_template("new.html", url = url, title = title)
+    link = Link()
+    link.title = title
+    link.url = url
+    link.comment = comment
+    link.create_at  = datetime.datetime.now()
+    db.session.add(link)
+    db.session.commit()
+    return render_template("close.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
